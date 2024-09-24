b7f94b776f0709bff3f83e44b072290442c608b1
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-09-24 23:38:04 +0800

    update

--- build.zig
+++ build.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const subcommander = b.addModule("subcommander", .{
-        .root_source_file = .{ .path = "./src/subcommander.zig" },
+        .root_source_file = b.path("./src/subcommander.zig"),
     });
 
     const examples = b.addTest(.{
-        .root_source_file = .{ .path = "./examples/examples.zig" },
+        .root_source_file = b.path("./examples/examples.zig"),
     });
     const run_examples = b.addRunArtifact(examples);
     const run_examples_step = b.step("test", "Run examples");
