6e067c46047da0ae8bb78a1c6f3ec9d0de7241ce
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-09-28 17:50:12 +0800

    rework

--- examples/examples.zig
+++ examples/examples.zig
@@ -4,6 +4,7 @@ const subcommander = @import("subcommander");
 test "match 1 command" {
     const mycommands: subcommander.Command = .{
         .match = "hello",
+        .execute = hello,
     };
     try mycommands.run(&.{"hello"});
 }
@@ -15,18 +16,42 @@ test "did not match command " {
     try std.testing.expectError(error.CommandNotFound, mycommands.run(&.{"world"}));
 }
 
-fn exeFn1(input: *const subcommander.InputCommand) void {
-    std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
-    std.testing.expectEqualSlices(u8, "world", std.mem.span(input.next.?.name)) catch unreachable;
-}
-
 test "match 2 and execute" {
     const mycommands: subcommander.Command = .{
         .match = "hello",
         .subcommands = &.{.{
             .match = "world",
-            .execute = exeFn1,
+            .execute = helloWorld,
         }},
     };
-    try mycommands.run(&.{"hello"});
+    try mycommands.run(&.{ "hello", "world" });
+    try std.testing.expectError(error.CommandNotFound, mycommands.run(&.{"hello"}));
+}
+
+test "match split path and execute" {
+    const mycommands: subcommander.Command = .{
+        .match = "hello",
+        .subcommands = &.{ .{
+            .match = "world",
+            .execute = helloWorld,
+        }, .{
+            .match = "foo",
+            .execute = helloFoo,
+        } },
+    };
+    try mycommands.run(&.{ "hello", "foo" });
+}
+
+fn helloWorld(input: *const subcommander.InputCommand) void {
+    std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
+    std.testing.expectEqualSlices(u8, "world", std.mem.span(input.prev.?.name)) catch unreachable;
+}
+
+fn helloFoo(input: *const subcommander.InputCommand) void {
+    std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
+    std.testing.expectEqualSlices(u8, "foo", std.mem.span(input.prev.?.name)) catch unreachable;
+}
+
+fn hello(input: *const subcommander.InputCommand) void {
+    std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
 }
--- src/subcommander.zig
+++ src/subcommander.zig
@@ -2,26 +2,20 @@ const std = @import("std");
 const print = std.debug.print;
 const fmt = @import("tree-fmt").defaultFormatter();
 
-/// Declaration
-pub fn notImplemented(_: *const InputCommand) void {
-    std.log.debug("this command is not implemented\n", .{});
-    return;
-}
-
 /// represents all possible commands
 pub const Command = struct {
     match: ?[*:0]const u8 = null,
     flags: []const Flags = &.{},
     subcommands: []const Command = &.{},
     description: []const u8 = &.{},
-    execute: fn (input: *const InputCommand) void = notImplemented,
+    execute: ?fn (input: *const InputCommand) void = null,
 
     pub fn run(
         self: Command,
         args: []const [*:0]const u8,
     ) !void {
         var input: InputCommand = .{ .name = undefined };
-        try self.run_rec(
+        try self.run_rec_command(
             // allocator,
             args,
             &input,
@@ -29,17 +23,21 @@ pub const Command = struct {
         );
     }
 
-    fn run_rec(
+    fn executeCmd(cmd: Command, input: *const InputCommand) !void {
+        // TODO: pretty print hints
+        const exeFn = cmd.execute orelse return error.CommandNotFound;
+        exeFn(input);
+    }
+
+    fn run_rec_command(
         self: Command,
         remain_args: []const [*:0]const u8,
         parent: *InputCommand,
         current: *InputCommand,
     ) !void {
-        if (remain_args.len == 0) return self.execute(parent);
+        if (remain_args.len == 0) return self.executeCmd(parent);
 
         var modified_args = remain_args;
-
-        // match command
         const next_arg = modified_args[0];
         if (self.match) |match| {
             if (memEqlSentinelStr(match, next_arg)) {
@@ -48,25 +46,25 @@ pub const Command = struct {
             }
         }
 
-        // match flags
-        // TODO: impl
-        //
-        //
+        return self.run_rec_flags(modified_args, parent, current);
+    }
 
-        if (modified_args.len == 0) {
-            return self.execute(current);
-        }
-
+    fn run_rec_sub_command(
+        self: Command,
+        remain_args: []const [*:0]const u8,
+        parent: *InputCommand,
+        current: *InputCommand,
+    ) !void {
         inline for (self.subcommands) |subcommand| {
             var child: InputCommand = .{ .name = undefined };
-            current.next = &child;
+            current.prev = &child;
             const done = blk: {
-                subcommand.run_rec(
+                subcommand.run_rec_command(
                     remain_args,
                     parent,
                     &child,
                 ) catch |err| {
-                    std.log.debug("subcommand rec error: {}\n", .{err});
+                    std.log.debug("subcommand rec error: {any}\n", .{err});
                     break :blk false;
                 };
                 break :blk true;
@@ -74,6 +72,65 @@ pub const Command = struct {
             if (done) return;
         }
         return error.CommandNotFound;
+    }
+
+    fn run_rec_flags(
+        self: Command,
+        remain_args: []const [*:0]const u8,
+        parent: *InputCommand,
+        current: *InputCommand,
+    ) !void {
+        if (remain_args.len == 0) return self.executeCmd(parent);
+
+        var modified_args = remain_args;
+
+        var next_arg = modified_args[0];
+        if (next_arg[0] == '-') {
+            next_arg = next_arg[1..];
+            var is_long = false;
+            if (next_arg[0] == '-') {
+                next_arg = next_arg[1..];
+                is_long = true;
+            }
+
+            // get input flag name and value
+            var input_flag_name: []const u8 = undefined;
+            var value: ?[*:0]const u8 = undefined;
+            const eql_idx = idxOfPosSentinelStr(next_arg, '=');
+            if (eql_idx) |idx| {
+                input_flag_name = next_arg[0..idx];
+                value = next_arg[idx + 1 ..];
+            } else {
+                input_flag_name = std.mem.span(next_arg);
+                value = null;
+            }
+
+            // find flag
+            for (self.flags) |flag| {
+                const target_name = if (is_long) flag.long else flag.short;
+                if (memEqlSentinelStr(target_name, input_flag_name)) {
+                    current.flags = .{
+                        .name = flag.long,
+                        .value = value,
+                        .prev = current.flags,
+                    };
+                    return self.run_rec_command(
+                        modified_args[1..],
+                        parent,
+                        current,
+                    );
+                }
+            }
+
+            std.log.err("flag not found: {s}\n", .{input_flag_name});
+            return error.FlagNotFound;
+        }
+
+        return self.run_rec_sub_command(remain_args, parent, current);
+    }
+
+    pub fn help() !void {
+        return error.NotImplemented;
     }
 };
 
@@ -91,21 +148,15 @@ pub const Flags = struct {
     description: [*:0]const u8 = "",
 };
 
-// flags
-pub const MyFlag = union(enum) {
-    param1: bool,
-    param2: i8,
-};
-
 /// Parsed input
 pub const InputCommand = struct {
     name: [*:0]const u8,
     flags: ?*InputFlag = null,
-    next: ?*InputCommand = null,
+    prev: ?*InputCommand = null,
 };
 
 pub const InputFlag = struct {
-    name: [*:0]const u8,
+    name: [*:0]const u8, // long name
     value: ?[*:0]const u8 = null,
     next: ?*InputFlag = null,
 };
@@ -119,3 +170,14 @@ fn memEqlSentinelStr(a: [*:0]const u8, b: [*:0]const u8) bool {
         if (a_val == 0) return true;
     }
 }
+
+fn idxOfPosSentinelStr(haystack: [*:0]const u8, needle: u8) ?usize {
+    var i: usize = 0;
+    while (true) : (i += 1) {
+        const val = haystack[i];
+        if (val == 0) return null;
+        if (val == needle) return i;
+    }
+}
+
+// TODO: suggestion, help
