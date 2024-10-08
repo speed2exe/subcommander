59af77365f4980aa2a201afd981e53a1ade0b860
Zack Fu Zi Xiang <speed2exe@live.com.sg>
2024-10-09 00:47:29 +0800

    added more test cases

--- examples/examples.zig
+++ examples/examples.zig
@@ -48,22 +48,51 @@ test "hello world/foo" {
 fn helloWorldParam(input: *const subcommander.InputCommand) void {
     std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
     std.testing.expectEqualSlices(u8, "world", std.mem.span(input.next.?.name)) catch unreachable;
-    const hello_flags = input.flags.?;
-    std.testing.expectEqualSlices(u8, "param", std.mem.span(hello_flags.name)) catch unreachable;
-    std.testing.expectEqual(null, hello_flags.value) catch unreachable;
+    const param = input.flags.?;
+    std.testing.expectEqualSlices(u8, "param", std.mem.span(param.name)) catch unreachable;
+    std.testing.expectEqual(null, param.value) catch unreachable;
+    std.testing.expectEqual(null, param.prev) catch unreachable;
+}
+fn helloFooParam(input: *const subcommander.InputCommand) void {
+    std.testing.expectEqualSlices(u8, "hello", std.mem.span(input.name)) catch unreachable;
+    std.testing.expectEqualSlices(u8, "foo", std.mem.span(input.next.?.name)) catch unreachable;
+    const b_param = input.next.?.flags.?;
+    std.testing.expectEqualSlices(u8, "b_param", std.mem.span(b_param.name)) catch unreachable;
+    std.testing.expectEqualSlices(u8, "goodbye", std.mem.span(b_param.value.?)) catch unreachable;
+    const a_param = b_param.prev.?;
+    std.testing.expectEqualSlices(u8, "a_param", std.mem.span(a_param.name)) catch unreachable;
+    std.testing.expectEqualSlices(u8, "", std.mem.span(a_param.value.?)) catch unreachable;
+    std.testing.expectEqual(null, a_param.prev) catch unreachable;
 }
 test "hello world --param" {
     const mycommands: subcommander.Command = .{
         .match = "hello",
-        .subcommands = &.{.{
-            .match = "world",
-            .execute = helloWorldParam,
-        }},
+        .subcommands = &.{
+            .{
+                .match = "world",
+                .execute = helloWorldParam,
+            },
+            .{
+                .match = "foo",
+                .execute = helloFooParam,
+                .flags = &.{
+                    .{
+                        .short = "a",
+                        .long = "a_param",
+                    },
+                    .{
+                        .short = "b",
+                        .long = "b_param",
+                    },
+                },
+            },
+        },
         .flags = &.{.{
             .short = "p",
             .long = "param",
         }},
     };
-    // try mycommands.run(&.{ "hello", "--param", "world" });
+    try mycommands.run(&.{ "hello", "--param", "world" });
     try mycommands.run(&.{ "hello", "-p", "world" });
+    try mycommands.run(&.{ "hello", "foo", "-a=", "-b=goodbye" });
 }
